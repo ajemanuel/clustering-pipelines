4521864d78a76efc036e7974f8040799813db93e
ajemanuel <alan_emanuel@hms.harvard.edu>
2017-06-23 17:21:51 -0400

    added sorting to liic and made the autolog append instead of overwrite

--- writeAutoLog.py
+++ writeAutoLog.py
@@ -3,7 +3,7 @@ import datetime
 
 def writeAutoLog():
 	d = datetime.datetime.now()
-	with open('C:\\Data\\autoClusterLogs\\log'+d.strftime('%Y%m%d')+'.txt','w+') as f:
+	with open('C:\\Data\\autoClusterLogs\\log'+d.strftime('%Y%m%d')+'.txt','a+') as f:
 	# x gives date in local format and X gives time in local format
 		f.write('Ran Autocluster today, '+d.strftime('%x')+' at '+d.strftime('%X')+'.\n')
 	
