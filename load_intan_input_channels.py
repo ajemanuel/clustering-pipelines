09fc632621ebe534873cc4daafe86491c1a29f49
ajemanuel <alan_emanuel@hms.harvard.edu>
2017-08-23 16:29:29 -0400

    corrected channel mapping in buzsaki probe
    now accounts for both the channel change from the adapter and from the
    probe

--- load_intan_input_channels.py
+++ load_intan_input_channels.py
@@ -1,29 +1,30 @@
-import glob as glob
+import glob
 import re
 import numpy as np
 import sys
 
-def load_intan_input_channels():
+def load_intan_input_channels(matchString='*'):
 	#usage:  [di,ai] = load_intan_input_channels() with ai/di files in the path
 	#save digital inputs
 	p = re.compile('chan(\d+).di')  #extract digital input channel ids
-	channel_ids = set(p.findall(' '.join(sorted(glob.glob('*.di')))))
+	channel_ids = set(p.findall(' '.join(sorted(glob.glob(matchString+'.di')))))
 	#load in each channel and concatenate.
 	digital_inputs = {}
 	for ch in channel_ids:
-		files = sorted(glob.glob('*chan' + ch + '.di'))
+		files = sorted(glob.glob(matchString+'chan' + ch + '.di'))
 		for f in files:
+			print(f)
 			if ch in digital_inputs: #concatenation relies on alphabetical ordering corresponding to temporal order
 				digital_inputs[ch] = np.append(digital_inputs[ch],(np.fromfile(f, dtype=np.uint32)))
 			else:  #load first array
 				digital_inputs[ch] = np.fromfile(f, dtype=np.uint32)
 	#save analog inputs
 	p = re.compile('chan(\d+).ai')  #extract analog input channel ids
-	channel_ids = set(p.findall(' '.join(sorted(glob.glob('*.ai')))))
+	channel_ids = set(p.findall(' '.join(sorted(glob.glob(matchString+'.ai')))))
 	#load in each channel and concatenate.
 	analog_inputs = {}
 	for ch in channel_ids:
-		files = sorted(glob.glob('*chan' + ch + '.ai'))
+		files = sorted(glob.glob(matchString+'chan' + ch + '.ai'))
 		for f in files:
 			if ch in analog_inputs: #load first array
 				analog_inputs[ch] = np.append(analog_inputs[ch],(np.fromfile(f, dtype=np.float64)))  
